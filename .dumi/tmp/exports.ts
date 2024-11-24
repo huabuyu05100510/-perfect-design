faeb24fcc6dfef6172538065dd1f6169e41677fe
huabuyu05100510 <huabuyu0510@163.com>
2024-11-24 15:46:22 +0800

    init

--- .dumi/tmp/exports.ts
+++ .dumi/tmp/exports.ts
@@ -4,47 +4,12 @@
 export { defineApp } from './core/defineApp'
 export type { RuntimeConfig } from './core/defineApp'
 // @umijs/renderer-*
-export {
-	createBrowserHistory,
-	createHashHistory,
-	createMemoryHistory,
-	createSearchParams,
-	generatePath,
-	matchPath,
-	matchRoutes,
-	Navigate,
-	NavLink,
-	Outlet,
-	resolvePath,
-	useLocation,
-	useMatch,
-	useNavigate,
-	useOutlet,
-	useOutletContext,
-	useParams,
-	useResolvedPath,
-	useRoutes,
-	useSearchParams,
-	Helmet,
-	useAppData,
-	useSelectedRoutes,
-	useClientLoaderData,
-	useServerLoaderData,
-	renderClient,
-	__getRoot,
-	Link,
-	useRouteData,
-	__useFetcher,
-	withRouter
-} from '/Users/kkkkkk/Desktop/Development/perfect-design/node_modules/.pnpm/registry.npmmirror.com+@umijs+renderer-react@4.0.51_ef5jwxihqo6n7gxfmzogljlgcm/node_modules/@umijs/renderer-react'
-export type { History } from '/Users/kkkkkk/Desktop/Development/perfect-design/node_modules/.pnpm/registry.npmmirror.com+@umijs+renderer-react@4.0.51_ef5jwxihqo6n7gxfmzogljlgcm/node_modules/@umijs/renderer-react'
+export { createBrowserHistory, createHashHistory, createMemoryHistory, createSearchParams, generatePath, matchPath, matchRoutes, Navigate, NavLink, Outlet, resolvePath, useLocation, useMatch, useNavigate, useOutlet, useOutletContext, useParams, useResolvedPath, useRoutes, useSearchParams, Helmet, useAppData, useSelectedRoutes, useClientLoaderData, useServerLoaderData, renderClient, __getRoot, Link, useRouteData, __useFetcher, withRouter } from '/Users/huabuyu/Documents/perfect-design/node_modules/.pnpm/registry.npmmirror.com+@umijs+renderer-react@4.0.51_react-dom@18.1.0_react@18.1.0/node_modules/@umijs/renderer-react';
+export type {  History } from '/Users/huabuyu/Documents/perfect-design/node_modules/.pnpm/registry.npmmirror.com+@umijs+renderer-react@4.0.51_react-dom@18.1.0_react@18.1.0/node_modules/@umijs/renderer-react'
 // umi/client/client/plugin
-export {
-	ApplyPluginsType,
-	PluginManager
-} from '/Users/kkkkkk/Desktop/Development/perfect-design/node_modules/.pnpm/registry.npmmirror.com+umi@4.0.51_rdgvyipbmv3sg47flosry4j3ou/node_modules/umi/client/client/plugin.js'
-export { history, createHistory } from './core/history'
-export { terminal } from './core/terminal'
-export { TestBrowser } from './testBrowser'
+export { ApplyPluginsType, PluginManager } from '/Users/huabuyu/Documents/perfect-design/node_modules/.pnpm/registry.npmmirror.com+umi@4.0.51_@babel+core@7.21.0_@types+node@18.19.64_@types+react@18.0.2_ktxoviacdmq7gfz3iqwuuvdihe/node_modules/umi/client/client/plugin.js';
+export { history, createHistory } from './core/history';
+export { terminal } from './core/terminal';
+export { TestBrowser } from './testBrowser';
 // plugins
 // plugins types.d.ts
